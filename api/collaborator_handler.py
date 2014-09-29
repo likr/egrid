d2fa8f46a4e0b7475d278fb118376d657d81779a
Yosuke ONOUE <onoue@likr-lab.com>
2014-09-29 20:35:45 +0900

    Fix POST collaborator error

--- api/collaborator_handler.py
+++ api/collaborator_handler.py
@@ -10,7 +10,8 @@ class CollaboratorHandler(webapp2.RequestHandler):
         project = Project.get(project_id)
         collaborators = Collaborator.all()\
             .filter('project =', project)
-        content = json.dumps([c.to_dict() for c in collaborators if c.project.deleted_at is None])
+        content = json.dumps([c.to_dict() for c in collaborators
+                              if c.project.deleted_at is None])
         self.response.write(content)
 
     def post(self, project_id):
@@ -25,14 +26,12 @@ class CollaboratorHandler(webapp2.RequestHandler):
             .filter('project =', project)\
             .filter('user =', user)\
             .get()
-        if collaborator:
-            raise Exception()
-        collaborator = Collaborator(
-            project=project,
-            user=user,
-            is_manager=data.get('isManager'))
-        collaborator.put()
-
+        if not collaborator:  # XXX
+            collaborator = Collaborator(
+                project=project,
+                user=user,
+                is_manager=data.get('isManager'))
+            collaborator.put()
         content = json.dumps(collaborator.to_dict())
         self.response.write(content)
 
