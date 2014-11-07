ab5e5b5bb3a8c58221aab62f9ee8f0cf80a1466f
Yosuke ONOUE <onoue@likr-lab.com>
2014-11-08 00:20:52 +0900

    Clear repository

--- ts/app/app.ts
+++
@@ -1,3 +0,0 @@
-/// <reference path="../ts-definitions/DefinitelyTyped/angularjs/angular.d.ts"/>
-
-angular.module('egrid', ['ui.router', 'ui.bootstrap', 'pascalprecht.translate', 'ngDragDrop']);
--- ts/app/filters/pager.ts
+++
@@ -1,9 +0,0 @@
-/// <reference path="../../ts-definitions/DefinitelyTyped/angularjs/angular.d.ts"/>
-
-angular.module('egrid')
-  .filter('pager', () => {
-    return (input: any[], currentPage: number, itemsPerPage: number) => {
-      var begin = (currentPage - 1) * itemsPerPage;
-      return input.slice(begin, begin + itemsPerPage);
-    };
-  });
