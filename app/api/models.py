27589f88cd537e40ccf657ec98779f7a86b0a656
Yosuke ONOUE <onoue@likr-lab.com>
2015-01-17 16:56:48 +0900

    Fix error on old data

--- app/api/models.py
+++ app/api/models.py
@@ -75,7 +75,7 @@ class Analysis(EgridModel):
             'projectKey': str(self.project.key()),
             'nodes': data['nodes'],
             'links': data['links'],
-            'groups': data['groups']
+            'groups': data.get('groups', [])
         }
 
     def get_questionnaire(self):
